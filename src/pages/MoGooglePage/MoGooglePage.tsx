861f4f356119ccc843bf041a48f0cb598f1d4673
MartinlyMo <martin.ly@movember.com>
2023-07-26 15:42:05 +1000

    initial commit and setting up file structure

---
+++ src/pages/MoGooglePage/MoGooglePage.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import { StyledTitle } from "./MoGooglePage.styled";
+
+export default function GooglePage() {
+    return (
+        <>
+            <StyledTitle>Hello world</StyledTitle>
+        </>
+    )
+}
